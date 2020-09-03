ca4cfa659e7fa40891cde2fc989ea25428c87a22
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-09-03 01:14:05 -0400

    :green_heart: Completed args

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -16,30 +16,14 @@ has the following keys:
 """
 
 waypoints = [
-    {
-        "lat": 43,
-        "lon": -121,
-        "name": "a place"
-    },
-    {
-        "lat": 41,
-        "lon": -123,
-        "name": "another place"
-    },
-    {
-        "lat": 43,
-        "lon": -122,
-        "name": "a third place"
-    }
+    {"lat": 43, "lon": -121, "name": "a place"},
+    {"lat": 41, "lon": -123, "name": "another place"},
+    {"lat": 43, "lon": -122, "name": "a third place"},
 ]
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
-waypoints.append({
-    "lat": 48,
-    "lon": -120,
-    "name": "a fourth place"
-})
+waypoints.append({"lat": 48, "lon": -120, "name": "a fourth place"})
 
 print(waypoints)
 
@@ -49,10 +33,7 @@ print(waypoints)
 # waypoints list.
 
 # YOUR CODE HERE
-waypoints[0].update({
-    "lon": -130,
-    "name": 'not a real place'
-})
+waypoints[0].update({"lon": -130, "name": "not a real place"})
 
 print(waypoints)
 
@@ -63,7 +44,7 @@ print(waypoints)
 def print_list_of_dict(l):
     for d in l:
         for k, v in d.items():
-            print(f'{k}: {v}')
+            print(f"{k}: {v}")
 
 
 print_list_of_dict(waypoints)
